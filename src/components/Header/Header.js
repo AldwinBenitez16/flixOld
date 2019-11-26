5eb9f673f20e950d302ac4b20d03de21a89befbd
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-25 23:21:39 -0500

    Created Header Container
    - Holds all the navigation and base links for the app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+// Dependencies
+import React from 'react';
+
+// Components
+import Dropdown from './Sidedrawer/Dropdown/Dropdown';
+import Logo from './Logo/Logo';
+import Navigation from './Navigation/Navigation';
+import Search from './Search/Search';
+
+// CSS
+import styles from './Header.module.css';
+
+const navigation = () => {
+    return (
+        <div className={styles.Header}>
+            <Dropdown />
+            <Logo />
+            <Navigation />
+            <Search />
+        </div>
+    );
+};
+
+export default navigation;
+
+/** -- Destop
+ * -- Logo
+ * -- Nav Items
+ * -- Search
+ */
+
+ /** -- Mobile
+ * -- Nav Items (A Drawer)
+ * -- Logo
+ * -- Search
+ */
