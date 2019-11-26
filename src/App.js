482747963f53ed154d4ad50ef7dde9ef1f144fb2
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-25 23:20:08 -0500

    Made App Component a class

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React, { Component } from 'react';
 // HOC
 import Layout from './hoc/Layout/Layout';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        {routes}
-      </Layout>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Layout>
+          
+        </Layout>
+      </div>
+    );
+  };
+};
 
 export default App;
