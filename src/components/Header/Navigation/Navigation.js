d0a13b838d03d87da5c8df22ae51116b56c6703d
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-26 21:09:55 -0500

    Adds Navigation and styling
    - Contains the navigation for the project and some styling

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,10 +1,20 @@
 // Dependencies
 import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+// CSS
+import styles from './Navigation.module.css';
 
 const navigation = () => (
-    <div>
-
-    </div>
+    <nav className={styles.Navigation}>
+        <ul>
+            <NavigationItem path="/home">Home</NavigationItem>
+            <NavigationItem path="/movies">Movies</NavigationItem>
+            <NavigationItem path="/tvshows">Tv-Shows</NavigationItem>
+            <NavigationItem path="/explore">Explore</NavigationItem>
+            <NavigationItem path="/login">Login</NavigationItem>
+        </ul>
+    </nav>
 );
  
 export default navigation;
