5842987a48ea6ec2353398414978524906be4aa7
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-26 21:09:20 -0500

    Adds Navigation Presentation Component and styling
    - returns a Li's item and Navlink that has basic styling

---
+++ src/components/Header/Navigation/NavigationItem/NavigationItem.js
@@ -0,0 +1,16 @@
+// Dependencies
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+// CSS
+import styles from './NavigationItem.module.css';
+
+const navigationItem = (props) => {
+    return(
+        <li className={styles.NavigationItem}>
+            <NavLink to={props.path}>{props.children}</NavLink>
+        </li>
+    );
+};
+
+export default navigationItem;
