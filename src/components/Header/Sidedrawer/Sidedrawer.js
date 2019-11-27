2ea61d98572ae0772f7dcddf177323d00e0a68d4
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-26 22:58:02 -0500

    Adds Sidedrawer Component and styling
    - a side nav that appears on mobile devices

--- src/components/Header/Sidedrawer/Sidedrawer.js
+++ src/components/Header/Sidedrawer/Sidedrawer.js
@@ -1,9 +1,23 @@
 // Dependencies
 import React from 'react';
 
-const sidedrawer = () => (
+// Component
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Navigation from '../Navigation/Navigation';
+import Button from '../../UI/Button/Button';
+
+// CSS
+import styles from './Sidedrawer.module.css';
+
+const sidedrawer = (props) => (
     <div>
-        
+        <Backdrop show={props.show} clicked={props.close}/>
+        <div className={[styles.Sidedrawer, ( props.show ? styles.Open : styles.Close)].join(' ')}>
+            <Button
+                type="Danger"
+                clicked={props.close}>X</Button>
+            <Navigation navStyles={styles.NavigationMob}/>
+        </div>  
     </div>
 );
 
