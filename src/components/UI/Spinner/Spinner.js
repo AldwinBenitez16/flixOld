73796a007f8de0e28a0ce9ca34f4bbbe0f46e957
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-27 23:28:38 -0500

    Adds Spinner Component
    - A reusable component that shows a spinner

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,11 @@
+// Dependencies
+import React from 'react';
+
+// CSS
+import styles from './Spinner.module.css';
+ 
+const spinner = () => (
+    <div className={styles.ldsSpinner}></div>
+);
+
+export default spinner;
