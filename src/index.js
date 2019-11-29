543f04f80b0494e6f382ae14014c1c4c0144c36d
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-29 10:48:01 -0500

    Adds react-redux
    - Added the provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 // Components
 import App from './App';
@@ -13,10 +16,19 @@ import "slick-carousel/slick/slick-theme.css";
 import "./slick/slick.css";
 import './index.css';
 
+const rootReducer = combineReducers({
+
+});
+
+const composeEnhancers = process.env.NODE_ENV === 'developement' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>, 
     document.getElementById('root')
 );
 
