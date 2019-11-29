3a2ae0c289228c8426a49afd81a465fcfdf8321b
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-28 22:56:31 -0500

    Adds banner Component and titles to Sliders

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,38 +7,47 @@ import WithData from '../../hoc/withDataWrapper/withDataWrapper';
 
 // Components
 import Slider from '../../components/Home/Slider/Slider';
+import Banner from '../../components/Home/Banner/Banner';
+
+// CSS
+import styles from './Home.module.css';
 
 class Home extends Component {
+    state = {
+        showOverlay: true
+    }
+
+    closeOverlayHandler = () => {
+        this.setState({ showOverlay: false });
+    }
+
+    showOverlayHandler = () => {
+        this.setState({ showOverlay: true });
+    }
+
     render() {
         return(
-            <div>
-                --Banner
-                <WithData path={`/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
-                </WithData>
-                <WithData path={`/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
+            <div className={styles.Home}>
+                <WithData path={`/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`}>
+                    <Banner 
+                        show={this.state.showOverlay}
+                        closeOverlay={this.closeOverlayHandler}
+                        showOverlay={this.showOverlayHandler}/>
                 </WithData>
                 <WithData path={`/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
+                    <Slider title="Now Playing"/>
                 </WithData>
                 <WithData path={`/trending/movie/week?api_key=${apiKey}`}>
-                    <Slider />
+                    <Slider title="Trending Movies"/>
+                </WithData>
+                <WithData path={`/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`}>
+                    <Slider title="Upcoming"/>
                 </WithData>
                 <WithData path={`/trending/tv/week?api_key=${apiKey}`}>
-                    <Slider />
+                    <Slider title="Trending TV Shows"/>
                 </WithData>
-                <WithData path={`/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
-                </WithData>
-                <WithData path={`/tv/top_rated?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
-                </WithData>
-                <WithData path={`/tv/on_the_air?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
-                </WithData>
-                <WithData path={`/tv/airing_today?api_key=${apiKey}&language=en-US&page=1`}>
-                    <Slider />
+                <WithData path={`/tv/popular?api_key=${apiKey}&language=en-US&page=1`}>
+                    <Slider title="Popular Tv Shows"/>
                 </WithData>
             </div>
         );
