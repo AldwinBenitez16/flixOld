8f1f1b534984ae9e20238a8066894a10f013463c
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-29 10:51:00 -0500

    Addss InfoCard component and styling
    - Uses withDataWrapper and retrieves the specific movies/tv show using the router params props passed as query string

---
+++ src/components/Info/InfoCard/InfoCard.js
@@ -0,0 +1,48 @@
+// Dependencies
+import React, { Fragment } from 'react';
+
+// Components 
+import Spinner from '../../UI/Spinner/Spinner';
+
+// CSS
+import styles from './InfoCard.module.css';
+
+const InfoCard = (props) => {
+    let infoSummary = <Spinner />;
+    if(!props.loading && props.data !== null) {
+        let genreList = props.data.genres.map(genre => {
+            return <li>{genre.name}</li>
+        });
+        infoSummary = (
+            <Fragment>
+                <img src={`https://image.tmdb.org/t/p/w500/${props.data.poster_path}`}
+                alt={props.data.original_title}/>
+                <div className={styles.InfoData}>
+                    <h2>{props.data.original_title}</h2>
+                    <h3>Overview</h3>
+                    <p className={styles.Overview}>{props.data.overview}</p>
+                    <div className={styles.Genres}>
+                        <h4>Genres: </h4>
+                        <ul>
+                            {genreList}
+                        </ul>
+                    </div>
+                    <div className={styles.InfoBox}>
+                        <p>Popularity: {props.data.popularity}</p>
+                        <p>Release: {props.data.release_date}</p>
+                        <p>Vote Average: {props.data.vote_average}</p>
+                        <p>Runtime: {props.data.runtime} mins</p>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+    console.log(props.data);
+    return (
+        <div className={styles.InfoCard}>
+            {infoSummary}
+        </div>
+    );
+};
+
+export default InfoCard;
