c5097aff61bb0004732b502d8e065facad532673
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-28 22:59:39 -0500

    Adds Banner Component
    - Uses with data to retrive the now playing movies and displays the first available movie with a valid backdrop poster path

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,68 @@
 // Dependencies
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 
-const banner = (props) => (
-    <div>
+// Components
+import Spinner from '../../UI/Spinner/Spinner';
+import Button from '../../UI/Button/Button';
 
-    </div>
-);
+// CSS
+import styles from './Banner.module.css';
+
+const banner = (props) => {
+    let spinner = <Spinner />;
+    if(!props.loading && props.data !== null) {
+
+        let counter = 0;
+        while(!props.data.results[counter].backdrop_path) {
+            counter++;
+        }
+        const latest = props.data.results[counter];
+        let overlay = null;
+        
+        if(props.show) {
+            overlay = (
+                <div className={styles.Overlay}>
+                    <h2>{latest.original_title}</h2>
+                    <p>{latest.overview}</p>
+                    <div className={styles.Info}>
+                        <p>Popularity: {latest.popularity}</p>
+                        <p>Release: {latest.release_date}</p>
+                        <p>IMDb: {latest.vote_average}</p>
+                    </div>
+                    <Button
+                        addClass={[styles.UpdatedMini]}
+                        action="Exit"
+                        type="Mini"
+                        clicked={props.closeOverlay}>X</Button>
+                    <Button
+                        addClass={[styles.UpdatedMini, styles.MiniInfo]}
+                        action="View More"
+                        type="Mini" 
+                        clicked={() => {console.log('GIMME MORE')}}>i</Button>
+                </div>
+            );
+        }
+        spinner = (
+            <Fragment>
+                <img 
+                    src={`https://image.tmdb.org/t/p/w1280/${latest.backdrop_path}`}
+                    alt={latest.original_title}/>;
+                {!props.show ? 
+                <Button
+                    addClass={[styles.UpdatedMini, styles.MiniOpenOverlay]}
+                    action="Open Info Overlay"
+                    type="Mini"
+                    clicked={props.showOverlay}>+</Button> : null}
+                {overlay}
+            </Fragment>
+        );
+    } 
+
+    return(
+        <div className={styles.Banner}>
+            {spinner}
+        </div>
+    );
+};
 
 export default banner;
