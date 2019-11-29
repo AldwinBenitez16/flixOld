1e2f29f8430b781aa9cbbccfa4e62fdc0933cd2b
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-28 22:56:08 -0500

    Adds a shouldComponentUpdate Catch
    - the withData now only updates the children component or itself it the data changed or the show property on the banner changed

--- src/hoc/withDataWrapper/withDataWrapper.js
+++ src/hoc/withDataWrapper/withDataWrapper.js
@@ -13,13 +13,19 @@ class withData extends Component {
         this.setState({loading: true, err: null});
         axios.get(this.props.path)
             .then(res => {
-                console.log(res.data);
                 this.setState({loading: false, data: res.data});
             })
             .catch(err => {
-                console.log(err.message);
                 this.setState({loading: false, err: err});
             });
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(this.state.data !== nextState.data ||
+            this.props.children.props.show !== nextProps.children.props.show) {
+            return true;
+        }
+        return false;
     }
     
     render() {
