b3cfeaee9145724876373d24fc921a35ed90f98e
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-28 22:57:30 -0500

    Adds addClasses functionality
    - The button component now receives new classes in an array and adds it to the button, as well as a tooltip/title

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,11 +4,18 @@ import React from 'react';
 // CSS
 import styles from './Button.module.css';
 
-const button = (props) => (
-    <button 
-        className={[styles.Button, styles[props.type]].join(' ')}
-        onClick={props.clicked}
-        disabled={props.disabled}>{props.children}</button>
-);
+const button = (props) => {
+    let newClasses = null;
+    if(props.addClass !== undefined && props.addClass !== null) {
+        newClasses = props.addClass.join(' ');
+    }
+    return(
+        <button 
+            title={props.action}
+            className={[styles.Button, styles[props.type], newClasses].join(' ')}
+            onClick={props.clicked}
+            disabled={props.disabled}>{props.children}</button>
+    );
+};
 
 export default button;
