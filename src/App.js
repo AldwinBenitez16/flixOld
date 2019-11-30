fb5fb1ad424efed8b3df03627e1a172f78ab02dc
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-29 22:49:03 -0500

    Adds Rotes to home, movies, tv shows and explore

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React, { Component, Fragment } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 // Components
-import Home from './containers/Home/Home';
+import Home from './components/Home/Home';
+import Movies from './components/Movies/Movies';
+import TvShows from './components/TvShows/TvShows';
 import Info from './components/Info/Info';
+import Explore from './containers/Explore/Explore';
 
 // HOC
 import Layout from './hoc/Layout/Layout';
+import PageWrapper from './hoc/PageWrapper/PageWrapper';
 
 class App extends Component {
   render() {
@@ -15,7 +19,10 @@ class App extends Component {
     let routes = (
       <Fragment>
         <Switch>
-          <Route path="/home" component={Home}/>
+          <Route path="/home" render={(props) => <PageWrapper {...props} ><Home /></PageWrapper>}/>
+          <Route path="/movies" render={(props) => <PageWrapper {...props} ><Movies /></PageWrapper>}/>
+          <Route path="/tvshows" render={(props) => <PageWrapper {...props} ><TvShows /></PageWrapper>}/>
+          <Route path="/explore" component={Explore}/>
           <Route path="/:type/:title" component={Info}/>
           <Redirect from="/" exact to="/home" />
         </Switch>
