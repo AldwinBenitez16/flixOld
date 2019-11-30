5762951b11cdf8f9eedf48adfa69f58504e6d495
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-29 22:44:59 -0500

    Addds PageWrapper HOC
    - creates a basic page layout adding the banner handlers and a state

---
+++ src/hoc/PageWrapper/PageWrapper.js
@@ -0,0 +1,38 @@
+// Dependencies
+import React, { Component, Fragment } from 'react';
+
+class PageWrapper extends Component {
+    state = {
+        showOverlay: true
+    }
+
+    closeOverlayHandler = () => {
+        this.setState({ showOverlay: false });
+    }
+
+    showOverlayHandler = () => {
+        this.setState({ showOverlay: true });
+    }
+
+    viewInfoHandler = (type, title, movieId) => {
+        this.props.history.push(`/${type}/${title}?movieId=${movieId}`);
+    }
+
+    render() {
+        let newProps = {
+            show: this.state.showOverlay,
+            closeOverlay: this.closeOverlayHandler,
+            showOverlay: this.showOverlayHandler,
+            viewInfo: this.viewInfoHandler,
+            ...this.props
+        };
+        let updatedComponent = React.cloneElement(this.props.children, newProps);
+        return(
+            <Fragment>
+                {updatedComponent}
+            </Fragment>
+        );
+    }
+}
+
+export default PageWrapper;
