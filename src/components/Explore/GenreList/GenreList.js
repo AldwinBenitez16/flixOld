7daaf8fd3e9682a9c37c92aba0612a8039efa2c5
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-11-30 23:56:44 -0500

    Adds genrelist component and styling

--- src/components/Explore/GenreList/GenreList.js
+++ src/components/Explore/GenreList/GenreList.js
@@ -1,14 +1,31 @@
 // Dependencies
 import React from 'react';
 
+// Components
+import Spinner from '../../UI/Spinner/Spinner';
+
 // CSS
 import styles from './GenreList.module.css';
 
 const genreList = (props) => {
-    console.log(props.data);
+    let genres = null;
+    if(props.show) {
+        genres = <Spinner />;
+        if(!props.loading && props.data !== null) {
+            genres = (
+                props.data.genres.map(genre => {
+                    return (
+                        <div key={genre.id}>
+                            <h3>{genre.name}</h3>
+                        </div>
+                    );
+                })
+            );
+        }
+    }
     return (
-        <div className={styles.GenreList}>
-            
+        <div className={props.show ? styles.GenreList : styles.Hidden}>
+            {genres}
         </div>
     );
 };
