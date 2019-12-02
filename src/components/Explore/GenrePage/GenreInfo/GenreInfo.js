587b75ef0e43ab21de84856ac0dd2dbbe64983d6
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-01 21:24:11 -0500

    Adds GenreInfo Component (In Progress)

---
+++ src/components/Explore/GenrePage/GenreInfo/GenreInfo.js
@@ -0,0 +1,13 @@
+// Dependencies
+import React from 'react';
+
+const genreInfo = (props) => {
+    console.log(props.data);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default genreInfo;
