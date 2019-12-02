a2e152b351c21c2e6c177d18c0496dd4da430f02
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-02 11:01:42 -0500

    Turns GenrePage Component into a stateful Component

--- src/components/Explore/GenrePage/GenrePage.js
+++ src/components/Explore/GenrePage/GenrePage.js
@@ -1,5 +1,5 @@
 // Dependencies
-import React from 'react';
+import React, { Component } from 'react';
 import { apiKey } from '../../../shared/Axios/axios';
 import queryString from 'query-string';
 
@@ -8,23 +8,59 @@ import DataWrapper from '../../../hoc/DataWrapper/DataWrapper';
 
 // Components
 import GenreInfo from './GenreInfo/GenreInfo';
+import Spinner from '../../UI/Spinner/Spinner';
 
 // CSS
 import styles from './GenrePage.module.css';
 
-const genrePage = (props) => {
-    const queries = queryString.parse(props.location.search);
-    const id = queries.id;
-    const type = queries.type;
-    const genre = props.match.url.substring(1);
-return(
-        <div className={styles.GenrePage}>
-            <DataWrapper path={`/discover/${type}?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}`}>
-                <GenreInfo 
-                    title={genre}/>
-            </DataWrapper>
-        </div>
-    );
+class GenrePage extends Component {
+
+    state={
+        id: null,
+        type: null,
+        page: null,
+        title: null
+    }
+
+    componentDidMount() {
+        const queries = queryString.parse(this.props.location.search);
+        this.setState({
+            id: queries.id,
+            type: queries.type,
+            page: queries.page,
+            genre: this.props.match.url.substring(1)
+        });
+    }
+
+    shouldComponentUpdate() {
+        return true;
+    }
+
+    changePageHandler = (page, max) => {
+        if(page > 0 && page <= max) {
+            this.props.history.push(`/${this.state.genre}?id=${this.state.id}&type=${this.state.type}&page=${page}`);
+            this.setState({page});
+        }
+    }
+
+    render() {
+        let list = <Spinner />;
+        if(this.state.id && this.state.type && this.state.page) {
+            list = (
+                <DataWrapper path={`/discover/${this.state.type}?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${this.state.page}&with_genres=${this.state.id}`}>
+                    <GenreInfo 
+                        changePage={this.changePageHandler}
+                        title={this.state.genre}
+                        page={this.state.page}/>
+                </DataWrapper>
+            );
+        }
+        return(
+            <div key={this.state.page} className={styles.GenrePage}>
+                {list}
+            </div>
+        );
+    }
 };
 
-export default genrePage;
+export default GenrePage;
