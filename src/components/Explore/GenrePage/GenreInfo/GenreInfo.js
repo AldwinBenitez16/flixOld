af3fb2905c7d062015da9f464e05bd3b67d1f36a
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-01 23:58:51 -0500

    Adds GenreInfo Component and styling

--- src/components/Explore/GenrePage/GenreInfo/GenreInfo.js
+++ src/components/Explore/GenrePage/GenreInfo/GenreInfo.js
@@ -1,11 +1,29 @@
 // Dependencies
 import React from 'react';
 
-const genreInfo = (props) => {
-    console.log(props.data);
-    return (
-        <div>
+// CSS
+import styles from './GenreInfo.module.css';
 
+const genreInfo = (props) => {
+    let genreItems = null;
+    if(!props.loading && props.data !== null) {
+        genreItems = props.data.results.map(curr => {
+            return (
+                <div key={curr.id}>
+                    <img 
+                        src={`https://image.tmdb.org/t/p/w500/${curr.poster_path}`} 
+                        onError={(e) => {e.target.src='https://i.imgur.com/zwpr2vD.jpg'}}  
+                        alt={curr.title}/>
+                </div>
+            );
+        });
+    }
+    return (
+        <div className={styles.GenreInfo}>
+            <div className={styles.GenreTitle}>
+                <h2>{props.title}</h2>
+            </div>
+            {genreItems}
         </div>
     );
 };
