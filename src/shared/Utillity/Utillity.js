e6b97203dbacb827daf231979f8447c1baadddd2
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-02 23:28:51 -0500

    Adds auth reducer to the store as well as actions(In Progress)

--- src/shared/Utillity/Utillity.js
+++ src/shared/Utillity/Utillity.js
@@ -1,3 +1,33 @@
+export const updateObject = (oldObject, updatedProperties) => {
+    return {
+        ...oldObject,
+        ...updatedProperties
+    };
+};
+
+export const checkValidity = (value , rules) => {
+    let isValid = true;
+
+    if(rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if(rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if(rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if(rules.isMail) {
+        const pattern = /^(([a-zA-Z0-9]+)|([a-zA-Z0-9]+((?:[a-zA-Z0-9]+)|(?:\.[a-zA-Z0-9]+))*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-zA-Z]{2,6}(?:\.[a-zA-Z]{2})?)$)/;
+        isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+};
+
 export const ternary = (check, backup, alternative) => {
     console.log();
     if(check && alternative) {
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,15 @@
+// ActionTypes
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+
+};
+
+const reducer = (state=initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    };
+};
+
+export default reducer;
