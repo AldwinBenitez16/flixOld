314746d24e6bf9388f1cd80abb46a40d475b95f8
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-02 23:09:00 -0500

    Adds Login Route and refracted genre and info routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import TvShows from './components/TvShows/TvShows';
 import Explore from './containers/Explore/Explore';
 import Info from './components/Info/Info';
 import GenrePage from './components/Explore/GenrePage/GenrePage';
+import Authentication from './components/Authentication/Authentication';
 
 // HOC
 import Layout from './hoc/Layout/Layout';
@@ -20,13 +21,15 @@ class App extends Component {
     let routes = (
       <Fragment>
         <Switch>
+          <Redirect from="/" exact to="/home" />
           <Route path="/home" render={(props) => <PageWrapper {...props} ><Home /></PageWrapper>}/>
           <Route path="/movies" render={(props) => <PageWrapper {...props} ><Movies /></PageWrapper>}/>
           <Route path="/tvshows" render={(props) => <PageWrapper {...props} ><TvShows /></PageWrapper>}/>
           <Route path="/explore" component={Explore}/>
-          <Route path="/:type/:title" component={Info}/>
-          <Route path="/:genre" component={GenrePage}/>
-          <Redirect from="/" exact to="/home" />
+          <Route path="/login" component={Authentication}/>
+          <Route path="/genre/:genre" render={(props) => <PageWrapper {...props}  ><GenrePage /></PageWrapper>} />
+          <Route path="/info/:type/:title" component={Info}/>
+          <Route render={() => <h2>Page Not Found!</h2>}/>
         </Switch>
       </Fragment>
     );
