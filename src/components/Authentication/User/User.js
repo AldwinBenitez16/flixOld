88132cc487d588ecd5ef4a901e2f2a73f5177e62
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-03 22:37:33 -0500

    Adds User Component (In Progress)

---
+++ src/components/Authentication/User/User.js
@@ -0,0 +1,15 @@
+// Dependencies
+import React from 'react';
+
+// CSS
+import styles from './User.module.css';
+
+const User = () => {
+    return (
+        <div className={styles.User}>
+            ---
+        </div>
+    );
+};
+
+export default User;
