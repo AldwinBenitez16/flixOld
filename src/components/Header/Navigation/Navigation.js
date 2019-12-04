a436971cdb022f95d9d4a923850aa333c37f5784
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-03 22:36:17 -0500

    Adds Login and Logout NavigationItems to Navigation Component

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,17 +1,40 @@
 // Dependencies
 import React from 'react';
+import { connect } from 'react-redux';
+
+// Components
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigation = (props) => (
-    <nav className={props.navStyles}>
-        <ul>
-            <NavigationItem path="/home">Home</NavigationItem>
-            <NavigationItem path="/movies">Movies</NavigationItem>
-            <NavigationItem path="/tvshows">Tv-Shows</NavigationItem>
-            <NavigationItem path="/explore">Explore</NavigationItem>
+// CSS
+import styles from './Navigation.module.css';
+
+const navigation = (props) => {
+    let loginNav = (
+        <div className={styles.Mobile}>
             <NavigationItem path="/login">Login</NavigationItem>
-        </ul>
-    </nav>
-);
- 
-export default navigation;
+        </div>
+    );
+    if(props.isAuth) {
+        loginNav = <NavigationItem path="/user">{props.IdData.username}</NavigationItem>;
+    }
+    return(
+        <nav className={props.navStyles}>
+            <ul>
+                <NavigationItem path="/home">Home</NavigationItem>
+                <NavigationItem path="/movies">Movies</NavigationItem>
+                <NavigationItem path="/tvshows">Tv-Shows</NavigationItem>
+                <NavigationItem path="/explore">Explore</NavigationItem>
+                {loginNav}
+            </ul>
+        </nav>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        isAuth: state.auth.authenticated,
+        IdData: state.auth.sessionIdData
+    };
+};
+
+export default connect(mapStateToProps)(navigation);
