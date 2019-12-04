1ad16097adba763f2c87115fa8a7db55faba1cdf
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-03 22:33:26 -0500

    Adds Cases for fetching token adn session Id

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -2,11 +2,59 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-
+    authenticated: false,
+    loading: false,
+    tokenData: null,
+    sessionIdData: null,
+    error: null
 };
 
 const reducer = (state=initialState, action) => {
     switch(action.type) {
+        case actionTypes.FETCH_TOKEN:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case actionTypes.FETCH_TOKEN_SUCCESS:
+                return {
+                    ...state,
+                    loading: false,
+                    tokenData: action.data
+                };
+        case actionTypes.FETCH_TOKEN_FAIL:
+                return {
+                    ...state,
+                    loading: false,
+                    error: action.error
+                };
+        case actionTypes.FETCH_SESSION_ID:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case actionTypes.FETCH_SESSION_ID_SUCCESS:
+                return {
+                    ...state,
+                    loading: false,
+                    authenticated: true,
+                    sessionIdData: action.data
+                };
+        case actionTypes.FETCH_SESSION_ID_FAIL:
+                return {
+                    ...state,
+                    loading: false,
+                    error: action.error
+                };
+        case actionTypes.LOGOUT:
+                return {
+                    ...state,
+                    authenticated: false,
+                    tokenData: null,
+                    sessionIdData: null
+                };
         default: 
             return state;
     };
