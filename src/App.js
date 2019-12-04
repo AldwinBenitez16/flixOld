cd31f223ad93e61e4555683e54eb497a5673c783
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-03 22:39:17 -0500

    Added New Route when Authenticated
    - Login
    - Logout
    - user

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Dependencies
 import React, { Component, Fragment } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 // Components
 import Home from './components/Home/Home';
@@ -10,6 +11,7 @@ import Explore from './containers/Explore/Explore';
 import Info from './components/Info/Info';
 import GenrePage from './components/Explore/GenrePage/GenrePage';
 import Authentication from './components/Authentication/Authentication';
+import LogOut from './components/Authentication/LogOut/LogOut';
 
 // HOC
 import Layout from './hoc/Layout/Layout';
@@ -34,6 +36,26 @@ class App extends Component {
       </Fragment>
     );
 
+    if(this.props.isAuth) {
+      routes = (
+        <Fragment>
+          <Switch>
+            <Redirect from="/" exact to="/home" />
+            <Route path="/home" render={(props) => <PageWrapper {...props} ><Home /></PageWrapper>}/>
+            <Route path="/movies" render={(props) => <PageWrapper {...props} ><Movies /></PageWrapper>}/>
+            <Route path="/tvshows" render={(props) => <PageWrapper {...props} ><TvShows /></PageWrapper>}/>
+            <Route path="/explore" component={Explore}/>
+            <Route path="/login" component={Authentication}/>
+            <Route path="/logout" component={LogOut}/>
+            <Route path="/user" render={() => <h2>USER AUTHENTICATED!!!</h2>} />
+            <Route path="/genre/:genre" render={(props) => <PageWrapper {...props}  ><GenrePage /></PageWrapper>} />
+            <Route path="/info/:type/:title" component={Info}/>
+            <Route render={() => <h2>Page Not Found!</h2>}/>
+          </Switch>
+        </Fragment>
+      );
+    }
+
     return(
       <div>
         <Layout>
@@ -44,4 +66,10 @@ class App extends Component {
   };
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.authenticated
+  };
+};
+
+export default connect(mapStateToProps)(App);
