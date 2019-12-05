6af694ecdc342e4e0b2164b4c1953ee936e25c0a
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-04 23:27:00 -0500

    Adds User Component

---
+++ src/containers/User/User.js
@@ -0,0 +1,90 @@
+// Dependencies
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
+import { updateObject } from '../../shared/Utillity/Utillity';
+
+// Components
+import Spinner from '../../components/UI/Spinner/Spinner';
+import Lists from '../../components/User/Lists/Lists';
+import Favorites from '../../components/User/Favorites/Favorites';
+import Rated from '../../components/User/Rated/Rated';
+import WatchList from '../../components/User/WatchList/WatchList';
+import UserControls from '../../components/User/UserControls/UserControls';
+
+// CSS
+import styles from './User.module.css';
+
+class User extends Component {
+
+    state = {
+        showLists: false,
+        showFavorites: false,
+        showWatchList: false,
+        showRated:false
+    }
+
+    componentDidMount() {
+        if(this.props.user.accountID === null) {
+            this.props.onFetchAccountDetails(this.props.sessionID);
+        }
+    }
+
+    showHandler = (type) => {
+        const show = updateObject(this.state, {
+            [`show${type}`]: true,
+            
+        });
+        this.setState({});
+    }
+
+    render() {
+        let user = <Spinner />;
+        if(this.props.user.accountID && !this.props.loading) {
+            user = (
+                <Fragment>
+                    <Lists 
+                        show={this.state.showLists}
+                        accountID={this.props.user.accountID}
+                        sessionID={this.props.sessionID}/>
+                    <Favorites 
+                        show={this.state.showFavorites}
+                        accountID={this.props.user.accountID}
+                        sessionID={this.props.sessionID}/>
+                    <Rated 
+                        show={this.state.showRated}
+                        accountID={this.props.user.accountID}
+                        sessionID={this.props.sessionID}/>
+                    <WatchList 
+                        show={this.state.showWatchList}
+                        accountID={this.props.user.accountID}
+                        sessionID={this.props.sessionID}/>
+                </Fragment>
+            );
+        }
+        return(
+            <div className={styles.User}>
+                <UserControls 
+                    />
+                {user}
+            </div>
+        );
+    }
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+        accountID: state.user.accountID,
+        loading: state.user.loading,
+        sessionID: state.auth.sessionIdData.session_id
+    };
+};
+
+const mpaDispatchToProps = dispatch => {
+    return {
+        onFetchAccountDetails: (sessionID) => dispatch(actions.fetchAccountDetails(sessionID))
+    };
+};
+
+export default connect(mapStateToProps, mpaDispatchToProps)(User);
