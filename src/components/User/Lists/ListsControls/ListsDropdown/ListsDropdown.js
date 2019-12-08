658b6e5bf4f993792bcad6e52c09a482afce9988
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-07 21:38:18 -0500

    Adds ListsDrop Component
    - displays the lists

---
+++ src/components/User/Lists/ListsControls/ListsDropdown/ListsDropdown.js
@@ -0,0 +1,36 @@
+// Dependencies
+import React from 'react';
+import { connect } from 'react-redux';
+
+// CSS
+import styles from './ListsDropdown.module.css';
+
+const listsDropdown = (props) => {
+    
+    let visible = styles.Hide;
+    if(!props.show) {
+        visible = styles.Show;
+    }
+    let listsContent = props.lists.map(list => (
+            <li 
+                key={list.id} 
+                id={list.id}>{list.description}</li>
+        ));
+    return (
+        <div className={[styles.ListsDropdown, visible].join(' ')}>
+            <ul>
+                {listsContent}
+            </ul>
+        </div>
+    );;
+};
+
+// --- listItems clearList/deleteList
+
+const mapStateToProps = state => {
+    return {
+        lists: state.info.accountLists
+    };
+};
+
+export default connect(mapStateToProps)(listsDropdown);
