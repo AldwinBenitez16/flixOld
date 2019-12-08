69b28b693cdfcd86f056af3af354ef3a4b1e29dd
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-07 23:30:04 -0500

    Adds ListOverlay and styling

---
+++ src/components/Info/InfoCard/ListOverlay/ListOverlay.js
@@ -0,0 +1,25 @@
+// Dependencies
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Components
+import ListDropdown from '../../../User/Lists/ListsControls/ListsDropdown/ListsDropdown';
+
+// CSS
+import styles from './ListOverlay.module.css';
+
+const listOverlay = (props) => {
+    return (
+        <div className={styles.ListOverlay}>
+            <ListDropdown show={true} lists={props.accountLists}/>
+        </div>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        lists: state.info.accountLists
+    };
+};
+
+export default connect(mapStateToProps)(listOverlay);
