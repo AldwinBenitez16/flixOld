ad2d691956229ae14f2c7400e805bed5050b7e45
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-07 21:36:47 -0500

    Adds accountlists state key and FetchAccountLists cases

--- src/store/reducers/info.js
+++ src/store/reducers/info.js
@@ -2,6 +2,7 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
     accountState: {},
+    accountLists: null,
     loading: false,
     error: null
 };
@@ -32,12 +33,20 @@ const reducer = (state=initialState, action) => {
             return {
                 ...state,
                 accountState: null,
+                accountLists: null,
                 loading: false,
                 error: null
             }
+        case actionTypes.UPDATE_MEDIA_STATE:
+            return {
+                ...state, 
+                loading: true,
+                error: null
+            };
         case actionTypes.UPDATE_MEDIA_STATE_SUCCESS:
             return {
                 ...state,
+                loading: false,
                 accountState: {
                     ...state.accountState,
                     [`${action.id}`]: {
@@ -45,6 +54,30 @@ const reducer = (state=initialState, action) => {
                         ...action.data
                     }
                 }
+            };
+        case actionTypes.UPDATE_MEDIA_STATE_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.error
+            };
+        case actionTypes.FETCH_ACCOUNT_LISTS: 
+            return {
+                ...state,
+                loading: true,
+                error: null
+            };
+        case actionTypes.FETCH_ACCOUNT_LISTS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                accountLists: action.data
+            };
+        case actionTypes.FETCH_ACCOUNT_LISTS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.error
             };
         default:
             return state;
