155378b5bc60eb3f35bfbf3b3e7bce2afb5f9ba0
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-08 23:30:37 -0500

    Adds List Item and connects the component to the ListDropdown Component

--- src/components/User/Lists/ListsControls/ListsDropdown/ListsDropdown.js
+++ src/components/User/Lists/ListsControls/ListsDropdown/ListsDropdown.js
@@ -2,20 +2,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+// Components
+import ListItem from './ListItem/ListItem';
+
 // CSS
 import styles from './ListsDropdown.module.css';
 
 const listsDropdown = (props) => {
-    
     let visible = styles.Hide;
     if(props.show) {
         visible = styles.Show;
     }
-    let listsContent = props.lists.map(list => (
-            <li 
+
+    let listsContent = props.lists.map(list => {
+        let name = list.name;
+        if(props.listType === 'info') {
+            if(name.length >= 20) {
+                name = name.substring(0, 17) + '...';
+            }
+        }
+        return ( 
+            <ListItem 
                 key={list.id} 
-                id={list.id}>{list.description}</li>
-        ));
+                id={list.id}
+                type={props.listType}
+                mediaID={props.mediaID}>{name}</ListItem>
+        );
+    });
     return (
         <div className={[styles.ListsDropdown, visible].join(' ')}>
             <ul>
