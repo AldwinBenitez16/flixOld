c12e69189e8d65f158320c50a36a938242d0ac38
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-10 23:26:58 -0500

    Adds UserPage component

---
+++ src/components/User/UserPage/UserPage.js
@@ -0,0 +1,45 @@
+// Dependencies
+import React, { Fragment } from 'react';
+
+// Components
+import DataInfo from '../../UI/DataInfo/DataInfo';
+
+
+// HOC
+import DataWrapper from '../../../hoc/DataWrapper/DataWrapper';
+
+const userPage = (props) => {
+    let pageContent = null;
+    if(props.show) {
+        console.log(props);
+        pageContent = (
+            <Fragment>
+                <DataWrapper path={props.moviePath}>
+                    <DataInfo 
+                        changePage={() => {console.log('change the page man')}}
+                        title={props.title}
+                        page={props.page}
+                        viewInfo={props.viewInfo}
+                        type="movie"
+                        stylesType="user"/>
+                </DataWrapper>
+                <DataWrapper path={props.tvPath}>
+                    <DataInfo 
+                        changePage={() => {console.log('change the page man')}}
+                        title={props.title}
+                        page={props.page}
+                        viewInfo={props.viewInfo}
+                        type="tv" 
+                        stylesType="user"/>
+                </DataWrapper>
+            </Fragment>
+        );
+    }
+    return (
+        <div>
+            {pageContent}
+        </div>
+    );
+};
+
+export default userPage;
