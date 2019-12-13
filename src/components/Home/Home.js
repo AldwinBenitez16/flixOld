e2d336f7acfbd3ae265ba4cf202d5060e1fde779
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-13 16:49:43 -0500

    Removed Home Component Sliders

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,38 +15,7 @@ import styles from './Home.module.css';
 const home = (props) => {
     return(
         <div className={styles.Home}>
-            <DataWrapper path={`/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`}>
-                <Banner
-                    viewInfo={props.viewInfo} 
-                    show={props.show}
-                    closeOverlay={props.closeOverlay}
-                    showOverlay={props.showOverlay}/>
-            </DataWrapper>
-            <DataWrapper path={`/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`}>
-                <Slider
-                    viewInfo={props.viewInfo}  
-                    title="Now Playing"/>
-            </DataWrapper>
-            <DataWrapper path={`/trending/movie/week?api_key=${apiKey}`}>
-                <Slider
-                    viewInfo={props.viewInfo}  
-                    title="Trending Movies"/>
-            </DataWrapper>
-            <DataWrapper path={`/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`}>
-                <Slider
-                    viewInfo={props.viewInfo}  
-                    title="Upcoming"/>
-            </DataWrapper>
-            <DataWrapper path={`/trending/tv/week?api_key=${apiKey}`}>
-                <Slider
-                    viewInfo={props.viewInfo}  
-                    title="Trending TV Shows"/>
-            </DataWrapper>
-            <DataWrapper path={`/tv/popular?api_key=${apiKey}&language=en-US&page=1`}>
-                <Slider
-                    viewInfo={props.viewInfo}  
-                    title="Popular Tv Shows"/>
-            </DataWrapper>
+            --- search 
         </div>
     );
 }
