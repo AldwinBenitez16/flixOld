76fc41d3339138fcbbb15b67a54d5b300c8e5b06
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-15 23:39:29 -0500

    Refracted Userpage Component

--- src/components/User/UserPage/UserPage.js
+++ src/components/User/UserPage/UserPage.js
@@ -1,5 +1,6 @@
 // Dependencies
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
 
 // Components
 import DataInfo from '../../UI/DataInfo/DataInfo';
@@ -8,37 +9,63 @@ import DataInfo from '../../UI/DataInfo/DataInfo';
 // HOC
 import DataWrapper from '../../../hoc/DataWrapper/DataWrapper';
 
-const userPage = (props) => {
-    let pageContent = null;
-    if(props.show) {
-        pageContent = (
-            <Fragment>
-                <DataWrapper path={props.moviePath}>
+class UserPage extends Component {
+    render() {
+        const {
+            show,
+            moviePath,
+            tvPath,
+            title,
+            stateType,
+            viewInfo,
+            page,
+            mediaIsFetched,
+            mediaItems
+        } = this.props;
+        let pageContent = null;
+        if(show) {
+            pageContent = (
+                <Fragment>
                     <DataInfo 
+                        loading={!mediaIsFetched}
+                        data={{
+                            results: mediaItems[stateType]['movies']
+                        }}
                         changePage={() => {console.log('change the page man')}}
-                        title={props.title}
-                        page={props.page}
-                        viewInfo={props.viewInfo}
+                        title={title}
+                        page={page}
+                        viewInfo={viewInfo}
                         type="movie"
-                        stylesType="user"/>
-                </DataWrapper>
-                <DataWrapper path={props.tvPath}>
+                        stylesType="user"
+                        stateType={stateType}/>
                     <DataInfo 
+                        loading={!mediaIsFetched}
+                        data={{
+                            results: mediaItems[stateType]['tv']
+                        }}
                         changePage={() => {console.log('change the page man')}}
-                        title={props.title}
-                        page={props.page}
-                        viewInfo={props.viewInfo}
+                        title={title}
+                        page={page}
+                        viewInfo={viewInfo}
                         type="tv" 
-                        stylesType="user"/>
-                </DataWrapper>
-            </Fragment>
+                        stylesType="user"
+                        stateType={stateType}/>
+                </Fragment>
+            );
+        }
+        return (
+            <div>
+                {pageContent}
+            </div>
         );
-    }
-    return (
-        <div>
-            {pageContent}
-        </div>
-    );
+    };
 };
 
-export default userPage;
+const mapStateToProps = state => {
+    return {
+        mediaIsFetched: state.info.mediaIsFetched,
+        mediaItems: state.info.mediaItems
+    };
+};
+
+export default connect(mapStateToProps)(UserPage);
