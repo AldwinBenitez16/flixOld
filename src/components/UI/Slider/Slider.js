297cb0ee6a4ae96d5dee31ad9eb744a3cf3ee2c2
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-15 23:38:57 -0500

    Refracted Slider Component

--- src/components/UI/Slider/Slider.js
+++ src/components/UI/Slider/Slider.js
@@ -1,5 +1,5 @@
 // Dependencies
-import React from 'react';
+import React, { Component } from 'react';
 import Slider from "react-slick";
 import { ternary } from '../../../shared/Utillity/Utillity';
 
@@ -9,46 +9,74 @@ import Spinner from '../../UI/Spinner/Spinner';
 // CSS
 import styles from './Slider.module.css';
 
-const slider = (props) => {
-    const settings = {
-        dots: false, 
-        infinite: true,
-        speed: 500,
-        slidesToShow: 8,
-        slidesToScroll: 8
+class SliderContainer extends Component {
+
+    state = {
+        width: 0
+    };
+
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
     }
-    let data = (
-        <div className={styles.SpinnerSlider}>
-            <Spinner />
-        </div>
-    );
-    if(!props.loading && props.data !== null) {
-        data = (
-            <Slider {...settings}>
-                {props.data.results.map(curr => {
-                    let type = "movie";
-                    if(curr.original_name) {
-                        type = "tv";
-                    }
-                    return (
-                        <div key={curr.id} className={styles.Item}>
-                            <img
-                                onClick={() => props.viewInfo(type, ternary(curr.original_title, curr.original_name), curr.id)}
-                                src={`https://image.tmdb.org/t/p/w500/${curr.poster_path}`} 
-                                onError={(e) => {e.target.src='https://i.imgur.com/zwpr2vD.jpg'}} 
-                                alt={curr.title}/>
-                        </div>
-                    );
-                })}
-            </Slider>
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+      
+    updateWindowDimensions = () => {
+        this.setState({ width: window.innerWidth });
+    }
+
+    render() {
+        let width = this.state.width;
+        let devider = 250;
+        if(width < 768) {
+            devider = 200;
+        }
+        console.log(Math.floor(width / devider));
+        const settings = {
+            dots: false, 
+            infinite: true,
+            speed: 500,
+            slidesToShow: Math.floor(width / devider),
+            slidesToScroll: Math.floor(width / devider)
+        }
+        
+        let data = (
+            <div className={styles.SpinnerSlider}>
+                <Spinner />
+            </div>
         );
-    }
-    return(
-        <div className={styles.Slider}>
-            <h2>{props.title}</h2>
-            {data}
-        </div>
-    );
+    
+        if(!this.props.loading && this.props.data !== null) {
+            data = (
+                <Slider {...settings}>
+                    {this.props.data.results.map(curr => {
+                        let type = "movie";
+                        if(curr.original_name) {
+                            type = "tv";
+                        }
+                        return (
+                            <div key={curr.id} className={styles.Item}>
+                                <img
+                                    onClick={() => this.props.viewInfo(type, ternary(curr.original_title, curr.original_name), curr.id)}
+                                    src={`https://image.tmdb.org/t/p/w500/${curr.poster_path}`} 
+                                    onError={(e) => {e.target.src='https://i.imgur.com/zwpr2vD.jpg'}} 
+                                    alt={curr.title}/>
+                            </div>
+                        );
+                    })}
+                </Slider>
+            );
+        }
+        return(
+            <div className={styles.Slider}>
+                <h2>{this.props.title}</h2>
+                {data}
+            </div>
+        );
+    };
 };
 
 /**
@@ -56,4 +84,4 @@ const slider = (props) => {
  * !data -> loadingSpinner
  */
 
-export default slider;
+export default SliderContainer;
