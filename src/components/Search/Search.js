777f9407dc057fcb8f01a8186ddc669c6af0f9fc
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2019-12-22 02:34:35 -0500

    Removes Search Component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,94 +1,92 @@
-// Dependencies
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { capitalize } from '../../shared/Utillity/Utillity';
-import * as actions from '../../store/actions/index';
+// // Dependencies
+// import React, { Component, Fragment } from 'react';
+// import { connect } from 'react-redux';
+// import { capitalize } from '../../shared/Utillity/Utillity';
+// import * as actions from '../../store/actions/index';
 
-// Components
-import DataInfo from '../UI/DataInfo/DataInfo';
-import Spinner from '../UI/Spinner/Spinner';
-import SearchInput from '../Header/SearchInput/SearchInput';
+// // Components
+// import DataInfo from '../UI/DataInfo/DataInfo';
+// import Spinner from '../UI/Spinner/Spinner';
+// import SearchInput from '../Header/SearchInput/SearchInput';
 
-// HOC
-import DataWrapper from '../../hoc/DataWrapper/DataWrapper';
+// // HOC
+// import DataWrapper from '../../hoc/DataWrapper/DataWrapper';
 
-// CSS
-import styles from './Search.module.css';
-import searchStyles from '../../shared/Styles/Search.module.css';
+// // CSS
 
-class Search extends Component {   
-    changePageHandler = (page, max) => {
-        if(page > 0 && page <= max) {
-            this.props.onChangeSearchPage(page);
-        }
-    }
+// class Search extends Component {   
 
-    render() {
-        const { 
-            searchQuery,
-            previousSearch,
-            previousType,
-            searched,
-            path
-        } = this.props;
-        let searchContent = (
-            <div className={styles.Invalid}>
-                <Spinner />
-            </div>
-        );
+//     changePageHandler = (page, max) => {
+//         if(page > 0 && page <= max) {
+//             this.props.onChangeSearchPage(page);
+//         }
+//     }
 
-        if(searchQuery.length <= 0 && previousSearch.length <= 0) {
-            searchContent = (
-                <div className={styles.Invalid}>
-                    <p><strong>Invalid Search:</strong> Please Enter A Valid Search!</p>
-                </div>
-            );
-        }
-        if(!searched) {
-            searchContent = (
-                <div className={styles.Invalid}>
-                    <p><strong>Search</strong> for your favorite <strong>movies</strong> and <strong>tv shows!!!</strong></p>
-                </div>
-            );
-        }
-        if(path.length > 0 && previousSearch.length > 0) {
-            searchContent = (
-                <Fragment key={path}>
-                    <DataWrapper path={path}>
-                        <DataInfo 
-                            addClass={[styles.DataInfo]}
-                            title={`[${capitalize(previousType)}] Search: ${previousSearch}`}
-                            changePage={this.changePageHandler} />
-                    </DataWrapper>
-                </Fragment>
-            );
-        }
+//     render() {
+//         const { 
+//             searchQuery,
+//             previousSearch,
+//             previousType,
+//             searched,
+//             path
+//         } = this.props;
+//         let searchContent = (
+//             <div className={styles.Invalid}>
+//                 <Spinner />
+//             </div>
+//         );
 
-        return(
-            <div className={styles.Search}>
-                <div className={styles.SearchContainer} >
-                    <SearchInput addClass={[searchStyles.SearchDesk, searchStyles.MainSearch, styles.HomeSearch]} />
-                </div>
-                {searchContent}
-            </div>
-        ); 
-    };
-};
+//         if(searchQuery.length <= 0 && previousSearch.length <= 0) {
+//             searchContent = (
+//                 <div className={styles.Invalid}>
+//                     <p><strong>Invalid Search:</strong> Please Enter A Valid Search!</p>
+//                 </div>
+//             );
+//         }
+//         if(!searched) {
+//             searchContent = (
+//                 <div className={styles.Invalid}>
+//                     <p><strong>Search</strong> for your favorite <strong>movies</strong> and <strong>tv shows!!!</strong></p>
+//                 </div>
+//             );
+//         }
+//         if(path.length > 0 && previousSearch.length > 0) {
+//             searchContent = (
+//                 <Fragment key={path}>
+//                     <DataWrapper path={path}>
+//                         <DataInfo 
+//                             addClass={[styles.DataInfo]}
+//                             title={`[${capitalize(previousType)}] Search: ${previousSearch}`}
+//                             changePage={this.changePageHandler} />
+//                     </DataWrapper>
+//                 </Fragment>
+//             );
+//         }
 
-const mapStateToProps = state => {
-    return {
-        searchQuery: state.search.searchQuery,
-        previousSearch: state.search.previousSearch.search,
-        previousType: state.search.previousSearch.type,
-        searched: state.search.searched,
-        path: state.search.path
-    };
-};
+//         return(
+//             <div className={styles.Search}>
+//                 <div className={styles.SearchContainer} >
+//                     <SearchInput addClass={[searchStyles.SearchDesk, searchStyles.MainSearch, styles.HomeSearch]} />
+//                 </div>
+//             </div>
+//         ); 
+//     };
+// };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onChangeSearchPage: (page) => dispatch(actions.changeSearchPage(page))
-    };
-};
+// const mapStateToProps = state => {
+//     return {
+//         searchQuery: state.search.searchQuery,
+//         previousSearch: state.search.previousSearch.search,
+//         previousType: state.search.previousSearch.type,
+//         searched: state.search.searched,
+//         path: state.search.path
+//     };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+// const mapDispatchToProps = dispatch => {
+//     return {
+//         onChangeSearchPage: (page) => dispatch(actions.changeSearchPage(page))
+//     };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Search);
