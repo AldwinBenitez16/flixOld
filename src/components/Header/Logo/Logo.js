f7bf2e6904bb9ea6812bd7ee9ea82973c75c3bdd
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-01-06 14:04:58 -0500

    Refracted Logo and styling

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -11,7 +11,10 @@ import logoSvg from '../../../assets/images/svgs/logo.svg';
 const logo = () => (
     <div className={styles.Logo}>
         <Link to="/home">
-            <img src={logoSvg} alt='Flix app logo'/>
+            <p className={styles.LogoLetters}>
+                <span className={styles.First} >R<span className={styles.R_Extend}>eact</span></span>
+                <span className={styles.Second}>M<span className={styles.M_Extend}>edia</span><span className={styles.D_Mob}>D</span></span>
+            </p>
         </Link>
     </div>
 );
